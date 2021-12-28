27a3abc96432f4b1daf96ac86c5b3bf0b5ff0a03
Paritosh Kundani <paritosh.kundani@prutech.com>
2021-12-28 09:05:34 -0500

    Creating Resources

---
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            this._mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        // array key: 1,2,3
+        // composite key: key1=value1,key2=value2
+        // below ids is a array of Guids, use ArrayModelBinder to get that to work, so API can bind 
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
+        public IActionResult GetAuthorCollection([FromRoute]
+            [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = _courseLibraryRepository.GetAuthors(ids);
+
+            // if not all authors are found
+            if (ids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+
+            return Ok(authorsToReturn);
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            var authorEntities = _mapper.Map<IEnumerable<Author>>(authorCollection);
+            foreach(var author in authorEntities)
+            {
+                _courseLibraryRepository.AddAuthor(author);
+            }
+
+            _courseLibraryRepository.Save();
+
+            // post so return 201 CreatedAtRoute
+            var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+            return CreatedAtRoute(
+                "GetAuthorCollection", 
+                new { ids = idsAsString },
+                authorCollectionToReturn
+            );
+        }
+    }
+}
---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.ResourceParameters;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            this._mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        // httphead is similar to httpget, but does not include the body
+        // can be used to verify something route is still there, look for 200 status without
+        // checking for body, returns just response headers
+        // because we used ControllerBase it is able to infer mainCategory and searchQuery are from QueryString (before we changed it to AuthorResourceParameters)
+        [HttpGet()]
+        [HttpHead]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors([FromQuery] AuthorResourceParameters authorResourceParameters)
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorResourceParameters);
+            
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+        }
+
+        // :guid will force it to only take guid, incase there is another similar with int for example
+        [HttpGet("{authorId:guid}", Name = "GetAuthor")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+        }
+
+        // since we used [ApiController] it includes the bad request check by default:
+        // if (author == null)
+        // {
+        //  return BadRequest();
+        // }
+        [HttpPost]
+        public ActionResult<AuthorDto> CreateAuthor(AuthorForCreationDto author)
+        {
+            var authorEntity = _mapper.Map<Entities.Author>(author);
+            _courseLibraryRepository.AddAuthor(authorEntity);
+            _courseLibraryRepository.Save();
+
+            var authorToReturn = _mapper.Map<Models.AuthorDto>(authorEntity);
+            // return 201 created response
+            return CreatedAtRoute("GetAuthor", new { authorId = authorEntity.Id }, authorToReturn);
+        }
+
+        // will let the consumer know if they can get the resouce, post to it, delete it and so on
+        [HttpOptions]
+        public IActionResult GetAuthorOptions()
+        {
+            Response.Headers.Add("Allow", "GET,OPTIONS,POST");
+            return Ok();
+        }
+    }
+}
